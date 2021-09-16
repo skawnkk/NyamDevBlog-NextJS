8c367889779152a5335b4b3b3d9482fc704cc11b
Namju <skawn11114@gmail.com>
2021-09-16 15:14:01 +0900

    feat : basic tag & styling

--- pages/authors/me.js
+++ pages/authors/me.js
@@ -1,3 +1,13 @@
 export default function AboutMe() {
-  return <h3>Hello, My name is Namju!</h3>;
+  return (
+    <>
+      <h3>Hello, My name is Namju!</h3>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </>
+  );
 }
