cb87d471dc9af401e79d4f62e47fb825b617d534
namju.kim <skawn11114@gmail.com>
2025-05-13 10:59:44 +0900

    utils

---
+++ lib/const.ts
@@ -0,0 +1,2 @@
+export const POST_WIDTH = 300;
+export const POST_HEIGHT = 300;
---
+++ lib/utils/toggle-dialog.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+interface UseToggleDialogProps {
+  defaultOpen?: boolean;
+}
+export const useToggleDialog = ({
+  defaultOpen = false,
+}: UseToggleDialogProps) => {
+  const [open, setOpen] = useState(defaultOpen);
+
+  const toggleOpen = () => setOpen((prev) => !prev);
+
+  return { open, setOpen, toggleOpen };
+};
