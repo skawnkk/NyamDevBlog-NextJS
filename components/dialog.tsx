6ecc93844bb2a7e10b0d8cc4df23adab2a1c7b8b
namju.kim <skawn11114@gmail.com>
2025-05-15 16:42:45 +0900

    style: dialog 스타일 수정

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -7,16 +7,18 @@ interface BasicDialogProps {}
 
 function BasicDialog({ children }: PropsWithChildren<BasicDialogProps>) {
   const router = useRouter();
+
   const closeDialog = () => {
     router.back();
   };
+
   const ref = useOutsideClick<HTMLDivElement>(closeDialog, true);
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
-      <div ref={ref} className="bg-white p-4 rounded w-[400px]">
+      <div ref={ref} className="relative bg-white p-4 rounded w-[400px]">
         <button
-          className="absolute right-1 top-1 inline-flex size-[24px] appearance-none items-center justify-center rounded-full text-white bg-black/50 focus:outline-none"
+          className="absolute right-3 top-3 inline-flex size-[24px] appearance-none items-center justify-center rounded-full text-white bg-black/50 focus:outline-none"
           aria-label="Close"
           onClick={closeDialog}
         >
